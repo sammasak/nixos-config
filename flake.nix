715c82f80a77d884150e617ed2dbe9b725ccc154
sammasak <23168291+sammasak@users.noreply.github.com>
2026-01-11 11:49:55 +0100

    restructure add backup

--- flake.nix
+++ flake.nix
@@ -37,6 +37,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = "backup";  # Backup conflicting files instead of failing
             home-manager.extraSpecialArgs = { userConfig = users.lukas; };
           }
 
