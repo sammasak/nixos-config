ac1ebc1cdd34f53065358c93c567946cac956db2
sammasak <23168291+sammasak@users.noreply.github.com>
2026-02-14 23:05:58 +0100

    msi-ms7758: disable ghost Unknown-1 monitor in Hyprland
    Fixes black screen after login when simpledrm exposes an invisible monitor.

--- hosts/msi-ms7758/variables.nix
+++ hosts/msi-ms7758/variables.nix
@@ -28,7 +28,12 @@
 
   # Monitor configuration (Hyprland): apply defaults to all monitors.
   monitors = [
-    ",preferred,auto,1"
+    # On this host, the firmware framebuffer (simpledrm) shows up as a fake
+    # monitor ("Unknown-1"). If we don't disable it, Hyprland will often place
+    # the real HDMI monitor to the right and start on the invisible one,
+    # resulting in an apparent black screen after login.
+    "Unknown-1,disable"
+    "HDMI-A-1,preferred,auto,1"
   ];
 
   # Features
